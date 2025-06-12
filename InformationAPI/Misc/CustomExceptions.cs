8f5c935ae1ac29a75309196ea9bf8742255494da
AmilaDeAlwis <amilaalwis4@gmail.com>
2025-06-12 21:35:26 +0530

    CustomExceptions file created to handle exceptions later

---
+++ InformationAPI/Misc/CustomExceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InformationAPI.Misc
+{
+    public class DataAccessException : Exception
+    {
+        public DataAccessException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+
+    public class ServiceException : Exception
+    {
+        public ServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
